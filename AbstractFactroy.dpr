d200e504614b6815fdf7f79c2925f876db66f78c
Marcos Leonardo Martins <mleonardomartins@gmail.com>
2024-08-18 21:47:33 -0300

    Mudança no DPR

--- AbstractFactroy.dpr
+++ AbstractFactroy.dpr
@@ -2,7 +2,11 @@ program AbstractFactroy;
 
 uses
   Vcl.Forms,
-  View.Main in 'src\views\View.Main.pas' {FrmMain};
+  View.Main in 'src\views\View.Main.pas' {FrmMain},
+  Providers.Interfaces in 'src\providers\interfaces\Providers.Interfaces.pas',
+  Model.Notebooks in 'src\model\Model.Notebooks.pas',
+  Model.Desktops in 'src\model\Model.Desktops.pas',
+  Factory.Marcas in 'src\providers\factorys\Factory.Marcas.pas';
 
 {$R *.res}
 
